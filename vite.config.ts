fef9fb453c66cfa4b5b1ce3fc4f0944d4ec9039b
grzegorczykdev <grzegorczykdev@gmail.com>
2026-02-12 23:00:05 +0100

    Add non-blocking CSS loading to Vite configuration to improve performance and prevent render blocking

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,29 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+/** Makes CSS load non-blocking: uses media="print" + onload to prevent render blocking (fixes Lighthouse LCP) */
+function nonBlockingCss() {
+  return {
+    name: "non-blocking-css",
+    transformIndexHtml: {
+      order: "post",
+      handler(html) {
+        return html.replace(
+          /<link rel="stylesheet"[^>]*href="([^"]+)"[^>]*>/g,
+          '<link rel="stylesheet" media="print" href="$1" onload="this.media=\'all\'"><noscript><link rel="stylesheet" href="$1"></noscript>'
+        );
+      },
+    },
+  };
+}
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [react(), nonBlockingCss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
